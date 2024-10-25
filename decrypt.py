86d3b585d2ec000c6408b7766ff6d78900f0c9e9
Ashwanth Balaji <ashwanthbalajir@gmail.com>
2024-10-25 20:39:45 +0530

    first commit

---
+++ decrypt.py
@@ -0,0 +1,30 @@
+import os
+
+from cryptography.fernet import Fernet
+
+#change 1
+
+files = []
+
+for file in os.listdir():
+    if file == "voldemort.py" or file == "decrypt.py" or file == "thekey.key":
+        continue
+    else:
+        files.append(file)
+
+
+print(files)
+
+with open("thekey.key","rb") as fkey:
+    key = fkey.read()
+
+print(key)
+
+for efile in files:
+    with open(efile,"rb") as fdr:
+        contents = fdr.read()
+    decrypted_content = Fernet(key).decrypt(contents)
+    with open(efile,"wb") as fdw:
+        fdw.write(decrypted_content)
+
+print("Contents have been decrypted !!!")
